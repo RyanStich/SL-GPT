e7abcafdeecce2964ea95726580821621e6ed1cb
Ry <55109747+RyanStich@users.noreply.github.com>
2023-11-10 15:36:49 -0800

    GPT-Server

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const app = express();
+
+app.use(cors({origin: 'null'}))
+app.use(express.json())
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.post("/letschat", async(req,res) => {
+
+    try{
+        const{prompt } = req.body;
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo", 
+            messages: [{
+                "role": "user",
+                "content":`${prompt}`
+            }],
+            max_tokens: 300,
+            temperature: 0.5,
+            top_p: 1,
+            frequency_penalty: 0.75,
+            presence_penalty: 0
+        })
+        return res.status(200).json({
+    success: true,
+    data: response.data
+
+        });
+    } catch(error) {
+return res.status(400).json({
+    success: false,
+    error: error.response
+    ? error.response.data
+    : "Error"
+})
+    }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
