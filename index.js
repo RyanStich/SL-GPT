b9f00192cec0999cb49361ac1ce9f155f6e66f8e
Ry <55109747+RyanStich@users.noreply.github.com>
2023-11-13 00:27:39 -0800

    assistant

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
-const express = require("express");
-const cors = require("cors");
+// Import the required dependencies
 require("dotenv").config();
-
-const { Configuration, OpenAIApi } = require("openai");
-
-const app = express();
-
-app.use(cors({origin: 'null'}))
-app.use(express.json())
-
-const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
+const OpenAI = require("openai");
+const express = require('express');
+const cors = require('cors');
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const openai = new OpenAIApi(configuration);
+// Create an OpenAI connection
+const secretKey = process.env.OPEN_AI_KEY;
+const openai = new OpenAI({
+  apiKey: secretKey,
+});
 
-app.post("/letschat", async(req,res) => {
+// Create an Express app
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-    try{
-        const{ messages } = req.body;
-        const response = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo", 
-            messages: [
-                ...messages
-            ],
-            max_tokens: 300,
-            temperature: 0.5,
-            top_p: 1,
-            frequency_penalty: 0.75,
-            presence_penalty: 0
-        })
-        return res.status(200).json({
-    success: true,
-    data: response.data
+// Endpoint to handle chat requests from the frontend
+app.post('/letschat', async (req, res) => {
+    try {
+        const { messages } = req.body;
 
+        // Create an assistant
+        const assistant = await openai.beta.assistants.create({
+          name: "Math Tutor",
+          instructions: "You are a personal math tutor. Write and run code to answer math questions.",
+          tools: [{ type: "code_interpreter" }],
+          model: "gpt-4-1106-preview",
         });
-    } catch(error) {
-return res.status(400).json({
-    success: false,
-    error: error.response
-    ? error.response.data
-    : "Error"
-})
+
+        // Create a thread
+        const thread = await openai.beta.threads.create();
+
+        // Add user's message to the thread
+        for (const message of messages) {
+          await openai.beta.threads.messages.create(thread.id, {
+            role: message.role,
+            content: message.content,
+          });
+        }
+
+        // Run the assistant
+        const run = await openai.beta.threads.runs.create(thread.id, {
+          assistant_id: assistant.id,
+        });
+
+        // Retrieve the run status
+        let runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+
+        // Polling mechanism to check if runStatus is completed
+        while (runStatus.status !== "completed") {
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+          runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+        }
+
+        // Get the assistant's response
+        const messagesResponse = await openai.beta.threads.messages.list(thread.id);
+        const assistantResponse = messagesResponse.data.filter(
+          (message) => message.role === "assistant"
+        );
+
+        // Send back the assistant's response
+        res.status(200).json({ responses: assistantResponse });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
     }
 });
 
+// Start the server
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server listening on port ${port}`));
